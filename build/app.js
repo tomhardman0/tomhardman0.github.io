f2039e92566fec31f4120ae42b5f80772fcdb3f8
Tom Hardman <tomhardman0@gmail.com>
2016-02-03 22:22:30 +0000

    correct git ignore

---
+++ build/app.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    var interactions = (() => {
+
+        console.log('tomhardman0.github.io');
+    })
+
+    var controllers = [interactions];
+
+    controllers.forEach(ctrl => ctrl());
+
+}());
+//# sourceMappingURL=app.js.map
