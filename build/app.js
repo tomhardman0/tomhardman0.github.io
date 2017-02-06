502dc0e99fd54b3844be4dcb91efde38a64c9872
Tom Hardman <tomhardman0@gmail.com>
2017-02-06 22:39:26 +0000

    it

--- build/app.js
+++ build/app.js
@@ -1,13 +1,35 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  var intro = (function () {})
+    var intro = (function () {
+        var textElems = document.querySelectorAll('.js-text');
+        var string = 'War on the palaces.';
 
-  var controllers = [intro];
+        var updateText = function updateText(element, index) {
+            setTimeout(function () {
+                element.innerHTML = string.substring(0, index);
+            }, 70 * index);
+        };
 
-  controllers.forEach(function (ctrl) {
-    return ctrl();
-  });
+        var typeText = function typeText(element, index) {
+            for (var i = 0; i <= string.length; i++) {
+                updateText(element, i);
+            }
+
+            setTimeout(function () {
+                element.innerHTML = '';
+                Array.prototype.forEach.call(textElems, typeText);
+            }, 5000);
+        };
+
+        Array.prototype.forEach.call(textElems, typeText);
+    })
+
+    var controllers = [intro];
+
+    controllers.forEach(function (ctrl) {
+      return ctrl();
+    });
 
 }());
 //# sourceMappingURL=app.js.map
--- src/js/ctrls/intro.js
+++ src/js/ctrls/intro.js
@@ -1,3 +1,23 @@
 export default () => {
+    const textElems = document.querySelectorAll('.js-text');
+    const string = 'War on the palaces.';
 
+    const updateText = (element, index) => {
+        setTimeout(() => {
+            element.innerHTML = string.substring(0, index);
+        }, 70 * index);
+    };
+
+    const typeText = (element, index) => {
+        for (var i=0; i<= string.length; i++) {
+            updateText(element, i)
+        }
+
+        setTimeout(() => {
+            element.innerHTML = ''
+            Array.prototype.forEach.call(textElems, typeText);
+        }, 5000);
+    };
+
+    Array.prototype.forEach.call(textElems, typeText);
 }
